8f61e9fe27996ef1831d3be482895d5233fd9334
zhangxuedong <zxxxd1001@126.com>
2017-02-24 17:20:43 +0800

    [张雪冬] 用java语言实现事件委托模式

---
+++ read-ZXD_HOME/read-jar/src/main/java/com/company/eventDelegateModel/Notifier.java
@@ -0,0 +1,20 @@
+package com.company.eventDelegateModel;
+
+/**
+ * Created by zhangxuedong on 2017/2/20.
+ */
+public abstract class Notifier {
+
+    private EventHandler eventHandler=new EventHandler();
+
+    public EventHandler getEventHandler() {
+        return eventHandler;
+    }
+    public void setEventHandler(EventHandler eventHandler) {
+        this.eventHandler = eventHandler;
+    }
+    //增加需要帮忙放哨的学生
+    public abstract void addListener(Object object,String methodName,Object...args);
+    //告诉所有要帮忙放哨的学生：老师来了
+    public abstract void notifyX();
+}
