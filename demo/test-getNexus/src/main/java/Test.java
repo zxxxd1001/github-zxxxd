883120104de0f5e9a81c6d93df31fb46ac4ee8c7
zhangxuedong <zxxxd1001@126.com>
2017-12-21 15:45:11 +0800

    [张雪冬] nexus上传jar包代码

---
+++ demo/test-getNexus/src/main/java/Test.java
@@ -0,0 +1,5 @@
+/**
+ * Created by zhangxuedong on 2017/12/15.
+ */
+public class Test {
+}
---
+++ demo/test-publish/src/main/java/git/zxxd/Test.java
@@ -0,0 +1,10 @@
+package git.zxxd;
+
+/**
+ * Created by zhangxuedong on 2017/12/15.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("ASdasd");
+    }
+}
