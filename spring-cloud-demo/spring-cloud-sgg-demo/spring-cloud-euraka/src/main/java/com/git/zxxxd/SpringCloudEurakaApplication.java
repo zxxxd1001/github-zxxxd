bb90f4d081e61d8e0abda06c223f72caf1d962e2
zhangxuedong <zxxxd1001@126.com>
2021-07-09 10:16:21 +0800

    [张雪冬] 删除无用项目

--- spring-cloud-demo/spring-cloud-sgg-demo/spring-cloud-euraka/src/main/java/com/git/zxxxd/SpringCloudEurakaApplication.java
+++ spring-cloud-demo/spring-cloud-sgg-demo/spring-cloud-euraka/src/main/java/com/git/zxxxd/SpringCloudEurakaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 /**
  * 启用eureka服务,接收其他服务注册
+ * --spring.profiles.active=7001
  */
 @EnableEurekaServer
 public class SpringCloudEurakaApplication {
